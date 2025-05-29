5b826f39e26f3760b6f3e61390efdb7227685810
Winan <jnacarinoa1@upao.edu.pe>
2025-05-29 18:06:04 -0500

    Mejorando la respuesta del servidor flask

--- app.py
+++ app.py
@@ -219,5 +219,4 @@ def handle_exception(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug_mode = os.environ.get('FLASK_ENV') == 'development'
-    app.run(debug=debug_mode, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
